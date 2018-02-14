1ef25580d8161676fcc334bba5516a6e6ed374f1
Christian Berendt <berendt@betacloud-solutions.de>
2018-02-14 17:12:45 +0100

    Add travis integration

---
+++ scripts/validate.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# based on https://gitlab.cern.ch/cloud-infrastructure/mistral-workflows/blob/master/scripts/validate.sh
+
+code=0
+
+for file in workbooks/*.yml; do
+    [ -e "$file" ] || continue
+    echo "Validating $file"
+    error=$(openstack --os-cloud testbed workbook validate $file -f value -c Error | head -1)
+    if [ "$error" != "None" ]; then
+        echo "Validation failed on $file please check error '$error'"
+        code=1
+    fi
+done
+
+for file in workflows/*.yml; do
+    [ -e "$file" ] || continue
+    echo "Validating $file"
+    error=$(openstack --os-cloud testbed workflow validate $file -f value -c Error | head -1)
+    if [ "$error" != "None" ]; then
+        echo "Validation failed on $file please check error '$error'"
+        code=1
+    fi
+done
+
+echo "Validation finished with code: $code"
+exit $code
